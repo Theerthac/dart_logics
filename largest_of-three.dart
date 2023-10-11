d0e6d0b4bb43184bcb44ffaa31fcc62b85c2511d
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-10-11 15:04:04 +0530

    questions

---
+++ largest_of-three.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+List<int>a=[1,2,3]{
+  for(var i=0;i)
+}
+}
--- pos_or_negative.dart
+++ pos_or_negative.dart
@@ -1,3 +1,15 @@
 void main(List<String> args) {
-  
+  int a = 1;
+
+  if (a > 0) {
+    print('it is a positive number');
+  }
+  if (a < 0) {
+    print('it is a negative number');
+  }
+  if (a == 0) {
+    print('it is 0');
+  } else {
+    print('');
+  }
 }
